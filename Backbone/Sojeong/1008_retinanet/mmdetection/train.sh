797ffac22efa72533375bef74e13d5d3e1ee6e1f
Soojeoong <soojeoong43@gmail.com>
2024-10-08 18:45:18 +0900

    Fix: gitignore 수정

--- Backbone/Sojeong/1008_retinanet/mmdetection/train.sh
+++ Backbone/Sojeong/1008_retinanet/mmdetection/train.sh
@@ -6,25 +6,25 @@ wandb login $WANDB_API_KEY
 
 # WANDB_RUN_NAME
 MODEL="retinanet"
-RUN_SUFFIX="sj_1"
+RUN_SUFFIX="sj_2"
 WANDB_RUN_NAME="mm_${MODEL}_${RUN_SUFFIX}"
 
-# python train.py \
-#     --model $MODEL \
-#     --root '/data/ephemeral/home/data/dataset/' \
-#     --classes "General trash" "Paper" "Paper pack" "Metal" "Glass" "Plastic" "Styrofoam" "Plastic bag" "Battery" "Clothing" \
-#     --img_scale 512 512 \
-#     --samples_per_gpu 4 \
-#     --seed 2022 \
-#     --gpu_ids 0 \
-#     --num_classes 10 \
-#     --grad_clip_max_norm 35 \
-#     --grad_clip_norm_type 2 \
-#     --max_keep_ckpts 3 \
-#     --checkpoint_interval 1 \
-#     --wandb_project $WANDB_PROJECT \
-#     --wandb_run_name $WANDB_RUN_NAME \
-#     --wandb_entity $WANDB_ENTITY
+python train.py \
+    --model $MODEL \
+    --root '/data/ephemeral/home/data/dataset/' \
+    --classes "General trash" "Paper" "Paper pack" "Metal" "Glass" "Plastic" "Styrofoam" "Plastic bag" "Battery" "Clothing" \
+    --img_scale 512 512 \
+    --samples_per_gpu 4 \
+    --seed 2022 \
+    --gpu_ids 0 \
+    --num_classes 10 \
+    --grad_clip_max_norm 35 \
+    --grad_clip_norm_type 2 \
+    --max_keep_ckpts 3 \
+    --checkpoint_interval 1 \
+    --wandb_project $WANDB_PROJECT \
+    --wandb_run_name $WANDB_RUN_NAME \
+    --wandb_entity $WANDB_ENTITY
 
 
 
