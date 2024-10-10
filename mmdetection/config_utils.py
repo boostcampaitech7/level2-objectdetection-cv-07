916b00a43b7753e8c2c53ea002923f5f8ed4e438
Jungyeon <seocindy2002@gmail.com>
2024-10-11 01:22:04 +0900

    Fix : config 수정

--- mmdetection/config_utils.py
+++ mmdetection/config_utils.py
@@ -1,4 +1,5 @@
 from mmcv import Config
+import json
 from mmdet.utils import get_device
 
 def load_config(config_file):
@@ -22,7 +23,7 @@ def update_config(cfg, root, classes):
     cfg.data.test.img_prefix = root
     cfg.data.test.ann_file = f'{root}/test.json'
     cfg.data.test.pipeline[1]['img_scale'] = (512, 512)
-    cfg.data.samples_per_gpu = 4
+    cfg.data.samples_per_gpu = 4    
 
     cfg.seed = 2022
     cfg.gpu_ids = [0]
@@ -34,5 +35,12 @@ def update_config(cfg, root, classes):
     cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
     cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
     cfg.device = get_device()
+    
+    # cfg 객체를 딕셔너리로 변환
+    cfg_dict = cfg._cfg_dict.to_dict()
 
+    # JSON 파일로 저장 (포맷팅 포함)
+    with open('mmdetection/config.json', 'w') as f:
+        json.dump(cfg_dict, f, indent=4)
+        
     return cfg
