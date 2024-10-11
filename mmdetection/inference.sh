1a4961a5761dada94db2341bed6d1f8f7e046ec1
Jungyeon <seocindy2002@gmail.com>
2024-10-11 17:49:20 +0900

    Feat : ensemble module

--- mmdetection/inference.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG_PATH='mmdetection/configs/yolo/yolov3_d53_320_273e_coco.py'
-ROOT='../../../dataset/'
-EPOCH='latest'
-
-python mmdetection/inference.py --config_path $CONFIG_PATH --root $ROOT --epoch $EPOCH
-
-# chmod +x mmdetection/inference.sh
