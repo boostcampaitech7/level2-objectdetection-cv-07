1d28015475391cfceb6b3198bb98c145e742b198
Jungyeon <seocindy2002@gmail.com>
2024-10-17 11:41:40 +0900

    Fix : fix inference.py

--- Backbone/Jungyeon/mmdetection_v3/inference.py
+++ Backbone/Jungyeon/mmdetection_v3/inference.py
@@ -9,6 +9,13 @@ import numpy as np
 with open('work_dirs/coco_detection/test.bbox.json', 'r') as f:
     output = json.load(f)
     
+def coco_to_voc(coco_bbox):
+    x_min = coco_bbox[0]
+    y_min = coco_bbox[1]
+    x_max = coco_bbox[0] + coco_bbox[2]  # x_min + width
+    y_max = coco_bbox[1] + coco_bbox[3]  # y_min + height
+    return [x_min, y_min, x_max, y_max]
+    
 def inference(output):
     prediction_strings = []
     file_names = []
@@ -16,18 +23,17 @@ def inference(output):
     img_ids = coco.getImgIds()
 
     for id in range(len(img_ids)):
-        # class_num = 10
+        print(f'=======================current id:{id}=======================')
         image_info = coco.loadImgs(coco.getImgIds(imgIds=id))[0]
         prediction_string = ''
+        last_idx = 0
         for i, out in enumerate(output):
             if int(out['image_id']) == id:
-                prediction_string += str(out['category_id']) + ' ' + str(out['score']) + ' ' + str(out['bbox'][0]) + ' ' + str(out['bbox'][1]) + ' ' + str(out['bbox'][2]) + ' ' + str(out['bbox'][3]) + ' '
-                # for j in range(class_num):
-                #     for o in out[j]:
-                #         print(o)
-                #         break
-                        # prediction_string += str(out['category_id']) + ' ' + str(out['score']) + ' ' + str(out['bbox'][0]) + ' ' + str(out['bbox'][1]) + ' ' + str(
-                        #     out['bbox'][2]) + ' ' + str(out['bbox'][3]) + ' '
+                last_idx = i
+                pas_voc_bbox = coco_to_voc(out['bbox'])
+                prediction_string += str(out['category_id']) + ' ' + str(out['score']) + ' ' + str(pas_voc_bbox[0]) + ' ' + str(pas_voc_bbox[1]) + ' ' + str(pas_voc_bbox[2]) + ' ' + str(pas_voc_bbox[3]) + ' '
+        print(f'last index:{last_idx}')
+        print(f'finished:{id}')
         prediction_strings.append(prediction_string)
         file_names.append(image_info['file_name'])
 
@@ -36,9 +42,9 @@ def inference(output):
     print(len(prediction_strings))
     submission['PredictionString'] = prediction_strings
     submission['image_id'] = file_names
-    submission.to_csv(os.path.join('submisson', f'submission_{epoch}.csv'), index=None)
+    submission.to_csv(os.path.join('./', 'submission_pas.csv'), index=None)
     submission.head()
-    return submission
+    return
 
 if __name__ == '__main__':
     epoch = 'latest'
