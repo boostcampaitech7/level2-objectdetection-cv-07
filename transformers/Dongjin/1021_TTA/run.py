6dc79f86d5559037721344e4e5b4e7dd659ad968
Dongjin Lee <jinlee.april@gmail.com>
2024-10-22 22:13:01 +0900

    Chore : backup

--- transformers/Dongjin/1021_TTA/run.py
+++ transformers/Dongjin/1021_TTA/run.py
@@ -55,19 +55,7 @@ def main(model_rel_path):
 
     tta_transforms_list = [
             (TTA.Compose([TTA.horizontal_flip]), 'hflip'),
-            (TTA.Compose([TTA.identity]), 'identity')
         ]
-
-    select = 'valid'
-    save_dir_path = os.path.join(py_dir_path, f'result/TTA/{select}')
-    os.makedirs(save_dir_path, exist_ok=True)
-
-
-    if select == 'valid':
-        for tta_transforms, tta_info in tta_transforms_list:
-            save_path = f'{save_dir_path}/{run_name}_{tta_info}.csv'
-            save_path = utils.renew_if_path_exist(save_path)
-            test_eval(model, image_processor, coco_valid, valid, tta_transforms, save_path)
 
     select = 'test'
     save_dir_path = os.path.join(py_dir_path, f'result/TTA/{select}')
@@ -78,6 +66,21 @@ def main(model_rel_path):
             save_path = f'{save_dir_path}/{run_name}_{tta_info}.csv'
             save_path = utils.renew_if_path_exist(save_path)
             test_eval(model, image_processor, coco_test, test, tta_transforms, save_path)
+
+    # tta_transforms_list = [
+    #         (TTA.Compose([TTA.horizontal_flip]), 'hflip'),
+    #         (TTA.Compose([TTA.identity]), 'identity')
+    #     ]
+
+    select = 'valid'
+    save_dir_path = os.path.join(py_dir_path, f'result/TTA/{select}')
+    os.makedirs(save_dir_path, exist_ok=True)
+
+    if select == 'valid':
+        for tta_transforms, tta_info in tta_transforms_list:
+            save_path = f'{save_dir_path}/{run_name}_{tta_info}.csv'
+            save_path = utils.renew_if_path_exist(save_path)
+            test_eval(model, image_processor, coco_valid, valid, tta_transforms, save_path)
 
 if __name__ == '__main__':
     fold_path = '../1011_model_search/result/1015/jozhang97'
