af26b17ef3bb72847632625602a42e43db6b1f8d
sng-tory <sin12345jk77@naver.com>
2024-10-25 14:41:15 +0900

    feat : final detectron2

--- Backbone/Sojeong/1008_retinanet/mmdetection/inference.sh
+++
@@ -1,9 +0,0 @@
-MODEL="autoassign_r50_fpn_8x2_1x_coco"
-
-python inference.py \
-    --model $MODEL \
-    --root '/data/ephemeral/home/data/dataset/' \
-    --num_classes 10 \
-
-# chmod +x ./inference.sh
-# ./inference.sh
